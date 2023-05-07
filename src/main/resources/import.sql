de5a5ae32ca87d2ce2514ca18bc800a8bd3d3997
Kunjal Chauhan <48762169+kunjalchauhan8@users.noreply.github.com>
2023-05-07 12:30:44 +0100

    Commit #2
    Added Some Data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+INSERT INTO employees (id,first_name, last_name) VALUES (next value for  EMPLOYEE_GEN,'Steve','M');
+INSERT INTO skills (id,level,name,employee_id) VALUES (next value for  skill_gen,1,'JAVA',current value for  EMPLOYEE_GEN);
+INSERT INTO skills (id,level,name,employee_id) VALUES (next value for  skill_gen,2,'Communication',current value for  EMPLOYEE_GEN);
+
+
+INSERT INTO employees (id,first_name, last_name) VALUES (next value for  EMPLOYEE_GEN,'John', 'R');
+INSERT INTO skills (id,level,name,employee_id) VALUES (next value for  skill_gen,1,'Testing',current value for  EMPLOYEE_GEN);
+
+
+INSERT INTO employees (id,first_name, last_name) VALUES (next value for  EMPLOYEE_GEN,'Kevin', 'A');
+INSERT INTO skills (id,level,name,employee_id) VALUES (next value for  skill_gen,2,'Business',current value for  EMPLOYEE_GEN);
+INSERT INTO skills (id,level,name,employee_id) VALUES (next value for  skill_gen,3,'Management',current value for  EMPLOYEE_GEN);
+
+
+INSERT INTO employees (id,first_name, last_name) VALUES (next value for  EMPLOYEE_GEN,'Mark','b');
+INSERT INTO skills (id,level,name,employee_id) VALUES (next value for  skill_gen,4,'Data',current value for  EMPLOYEE_GEN);
